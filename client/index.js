a1a374b19b11bdfd5612fdc3d71da66e29762f74
huonglt <lthuongvn@yahoo.com>
2017-03-17 20:20:41 +1300

    remove node_modudles in last commit

--- client/index.js
+++ client/index.js
@@ -1,2 +1,6 @@
-require('babel-register');
-require('./pingPong.js');  
+import { store, runCycle } from './redux/store.js';
+import { PING_ACTION, PONG_ACTION } from './redux/actions.js';
+import { main } from './cycles/main.js';
+
+runCycle(main);
+store.dispatch(PING_ACTION);
