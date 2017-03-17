5dffe4c23aacc13858533a8c2aa206d8f1012b63
huonglt <lthuongvn@yahoo.com>
2017-03-17 20:23:37 +1300

    merged code in pingPong.js into index.js

--- client/pingPong.js
+++
@@ -1,6 +0,0 @@
-import { store, runCycle } from './redux/store.js';
-import { PING_ACTION, PONG_ACTION } from './redux/actions.js';
-import { main } from './cycles/main.js';
-
-runCycle(main);
-store.dispatch(PING_ACTION);
