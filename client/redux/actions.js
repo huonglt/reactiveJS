1e84b8de8c252652a987f6a0c42669c9b58c8896
huonglt <lthuongvn@yahoo.com>
2017-03-19 20:58:28 +1300

    add action and action creator for REQUEST_USER_REPOS and RECEIVED_USER_REPO

--- client/redux/actions.js
+++ client/redux/actions.js
@@ -1,2 +1,11 @@
 export const PING_ACTION = { type: 'PING' };
 export const PONG_ACTION = { type: 'PONG' };
+export const REQUEST_USER_REPOS_ACTION = { type: 'REQUEST_USER_REPO', payload: { user: 'huonglt' }};
+
+export const receivedUserRepoAction = (user, repos) => ({
+    type: 'RECEIVED_USER_REPOS',
+    payload: {
+      user,
+      repos
+    }
+});
