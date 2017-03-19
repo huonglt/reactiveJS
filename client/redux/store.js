4fa7d4a31f7720a050775ed59e2fd1774dedd4be
huonglt <lthuongvn@yahoo.com>
2017-03-19 21:00:19 +1300

    wrap logic to create store and setup cycle middleware in configStore function. the cycle will run when this function executes. Dont need to call run cycle on client/index.js

--- client/redux/store.js
+++ client/redux/store.js
@@ -2,9 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import { run } from '@cycle/run';
 import { createCycleMiddleware } from 'redux-cycles';
 import { reducers } from './reducers.js';
+import cycle from '../cycles/index.js';
+import { makeHTTPDriver } from '@cycle/http';
 
-const cycleMiddleware = createCycleMiddleware();
+/*
+ * config the redux store with cycles redux-cycles for handling HTTP request side-effects
+ */
+export const configStore = () => {
+  const cycleMiddleware = createCycleMiddleware();
+  const { makeActionDriver } = cycleMiddleware;
+  const store = createStore(reducers, applyMiddleware(cycleMiddleware));
+  const drivers = { ACTION: makeActionDriver(), HTTP: makeHTTPDriver() };
 
-export const store = createStore(reducers, applyMiddleware(cycleMiddleware));
-
-export const runCycleRx = (mainFn) => run(mainFn, { ACTION: cycleMiddleware.makeActionDriver()});
+  /*
+   * cycle function take input source from drivers, and output to drivers sink
+   * run to bind the cycle function with the drivers
+   */
+  run(cycle, drivers);
+  return store;
+}
